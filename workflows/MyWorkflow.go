cbd9a9279e32dca2ac250842f0845cb846a10ebe
GeetaAhuja2022 <geetu_ahuja@yahoo.com>
2024-10-14 23:03:27 -0500

    changes

--- workflows/MyWorkflow.go
+++ workflows/MyWorkflow.go
@@ -2,16 +2,34 @@ package workflows
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/GeetaAhuja2022/temporal-worker/activities"
 
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 )
 
 // MyWorkflow is a Temporal workflow that uses a factory to execute activities with a common return type.
 func MyWorkflow(ctx workflow.Context, input map[string]interface{}) (string, error) {
+
+	retryPolicy := &temporal.RetryPolicy{
+		InitialInterval:    time.Second,
+		BackoffCoefficient: 2.0,
+		MaximumInterval:    time.Minute,
+		MaximumAttempts:    3,
+	}
+
+	options := workflow.ActivityOptions{
+		// Timeout options specify when to automatically timeout Activity functions.
+		StartToCloseTimeout: time.Minute,
+		// Optionally provide a customized RetryPolicy.
+		// Temporal retries failures by default, this is just an example.
+		RetryPolicy: retryPolicy,
+	}
 	var result activities.ActivityResult
 	var err error
+	ctx = workflow.WithActivityOptions(ctx, options)
 
 	// Dynamically execute the correct activity based on type
 
